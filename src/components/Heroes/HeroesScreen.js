0462248dc3db7906dfc8127b06417e282af805bd
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-10-08 21:16:52 -0500

    heroscreen

--- src/components/Heroes/HeroesScreen.js
+++ src/components/Heroes/HeroesScreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useParams, Redirect } from 'react-router-dom';
+import { useParams, Redirect  } from 'react-router-dom';
 import {GetHeroesById } from '../../selectors/GetHeroesById';
 
 
-export const HeroesScreen = () => {
+export const HeroesScreen = ( { history } ) => {
 
 
-    
+
     const { heroeid } = useParams();
 
     const hero = GetHeroesById( heroeid );
@@ -15,6 +15,20 @@ export const HeroesScreen = () => {
 
     if( !hero ){
         return <Redirect to="/" />
+    }
+
+    console.log( history.length );
+
+    const handleReturn = () => {
+
+        if( history.length <= 2  ){
+            history.push('/');
+        }else{
+            history.goBack();
+        }
+
+
+
     }
 
 
@@ -29,8 +43,34 @@ export const HeroesScreen = () => {
 
 
     return (
-        <div>
-            <h1>HeoresScreen</h1>
+        <div  className="row mt-5" >
+
+            <div className="col-4" >
+                <img className="img-thumbnail"  src={ `../assets/img/${id}.jpg` }  alt={ superhero } />
+            </div>  
+
+            <div className="col-8" >
+                <h3>{  superhero }</h3>
+
+                    <ul className="list-group list-group-flush" >
+
+                        <li className="list-group-item" > Alter Ego:  { alter_ego } </li>
+                        <li className="list-group-item" > Publisher:  { publisher } </li>
+                        <li className="list-group-item" > first Appearance:  { first_appearance} </li>
+
+                    </ul>
+
+                    <h5> Characters </h5>
+
+                    <p> {  characters } </p>
+
+                    <button onClick={ handleReturn }  className="btn btn-outline-primary">Return</button>
+
+                    
+
+            </div>
+
         </div>
+
     )
 }
