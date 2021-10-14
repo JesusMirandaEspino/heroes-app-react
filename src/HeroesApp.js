d5b634553fb25d10c90f6106905a9abe609d5c72
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-10-13 20:49:50 -0500

    localstorage context

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { AppRouter } from './routers/AppRouter';
+import { authContext } from './auth/authContext';
+import { authReducer } from './auth/authReducer';
+
+const init = () => {
+    return JSON.parse( localStorage.getItem( 'user' ) ) || { logged: false }
+}
+
 
 export const HeroesApp = () => {
+
+    const [ user, dispatch] = useReducer( authReducer, {}, init)
+
     return (
-        <div>
+        <authContext.Provider  value={{  user, dispatch }}>
             <AppRouter />
-        </div>
+        </authContext.Provider>
     )
 }
