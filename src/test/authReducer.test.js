2b528e2c245fa954081025a2c23c7e12dc57b24b
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-10-15 21:42:27 -0500

    test authReducer

---
+++ src/test/authReducer.test.js
@@ -0,0 +1,45 @@
+import { authReducer } from '../auth/authReducer';
+import { types } from '../types/types';
+
+
+describe('Pruebes con authReducer', () => {
+
+
+    test('Debe de mostrar el estado por defecto ', () => {
+
+        const state = authReducer( { logged: false }, {} );
+
+        expect( state ).toEqual( { logged: false} );
+
+    });
+
+
+    test('Debe autenticar y colocar el name del usuario', () => {
+
+        const action = {
+            type: types.login,
+            payload: {
+                name: 'Jesus',
+            }
+
+        }
+
+        const state = authReducer( { logged: false }, action );
+
+        expect( state ).toEqual( { logged: true, name:'Jesus'} );
+
+
+    });
+
+    test('Debe de borrar el name del usuario y el logged en false', () => {
+        const action = {
+            type: types.logout
+        }
+
+        const state = authReducer( { logged: true, name: 'jesus' }, action );
+
+        expect( state ).toEqual( { logged: false } );
+
+    });
+
+});
