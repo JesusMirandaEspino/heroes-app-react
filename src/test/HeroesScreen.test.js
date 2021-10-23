36971e3ec0adcd6044cd5b604f43592150184de5
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-10-22 20:51:57 -0500

    test push & goBack

--- src/test/HeroesScreen.test.js
+++ src/test/HeroesScreen.test.js
@@ -34,7 +34,60 @@ describe('Pruebas con <HeroesScreen />', () => {
 
         expect( wrapper.find('div').exists() ).toBe(true);
 
-    })
+    });
+
+
+    test( 'Debe de regresar a la pantalla anterior con PUSH', () => {
+
+        const history = {
+            length: 1,
+            push: jest.fn(),
+            goBack: jest.fn()
+        };
+
+        const wrapper = mount(
+            <MemoryRouter  initialEntries={ ['/hero/marvel-spider'] }>
+                <Route path="/hero/:heroeid"  component={  () => <HeroesScreen  history={ history } /> }></Route>
+            </MemoryRouter>
+        );
+
+        wrapper.find('button').prop('onClick')();
+
+        expect( history.push ).toHaveBeenCalledWith('/');
+        expect( history.goBack ).not.toHaveBeenCalled();
+
+    });
+
+    test('Debe de regresar a la pantalla anterior ', () => {
+        
+        const wrapper = mount(
+            <MemoryRouter  initialEntries={ ['/hero/marvel-spider'] }>
+                <Route path="/hero/:heroeid"  component={  () => <HeroesScreen  history={ history } /> }></Route>
+            </MemoryRouter>
+        );
+
+        wrapper.find('button').prop('onClick')();
+
+        expect( history.goBack ).toHaveBeenCalled();
+        expect( history.push ).not.toHaveBeenCalled();
+
+
+    });
+
+
+    test('Debe de llamar al redirect si el hero no existe ', () => {
+        const wrapper = mount(
+            <MemoryRouter  initialEntries={ ['/hero/marvel-spider1256787154'] }>
+                <Route path="/hero/:heroeid"  component={  () => <HeroesScreen  history={ history } /> }></Route>
+            </MemoryRouter>
+        );
+
+            expect( wrapper.text() ).toBe('');
+
+
+    });
+    
+    
     
     
 
